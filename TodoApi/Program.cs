edccc5be76b08400ed1216c525bca18903222823
anton-ont <56735447+anton-ont@users.noreply.github.com>
2019-11-17 17:37:37 -0500

    Update Program.cs

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -14,6 +14,7 @@ namespace TodoApi
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            // Code was added in GitHub
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
